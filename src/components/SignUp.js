bb6c04958791b6dc02b190a5da80653057534bc0
jguz17 <johnathanguzman17@gmail.com>
2020-07-19 19:08:19 -0500

    responsive ready(i hope lolol)

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -28,7 +28,7 @@ class SignUp extends Component {
           <h3>Signup</h3>
           <input className='signup-input' name='username' value={username} onChange={this.handleChange} placeholder='username'/>
           <input className='signup-input' name='password' type='password' value={password} onChange={this.handleChange} placeholder='password'/>
-          <button id='signup-button' type='submit'>Signup & Login</button>
+          <button id='signup-button' type='submit'>Signup</button>
         </form>
       </div>
     );
